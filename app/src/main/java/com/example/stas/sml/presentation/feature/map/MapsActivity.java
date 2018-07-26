740763360780bf508b3021606b18b1f67190fb64
Stas Stahma <StasStahma@gmail.com>
2018-07-26 09:49:26 +0300

    PlacesActivity added

--- app/src/main/java/com/example/stas/sml/presentation/feature/map/MapsActivity.java
+++ app/src/main/java/com/example/stas/sml/presentation/feature/map/MapsActivity.java
@@ -1,39 +1,93 @@
 package com.example.stas.sml.presentation.feature.map;
 
-
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.RuntimePermissions;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.example.stas.sml.App;
-import com.example.stas.sml.MapsFragment;
 import com.example.stas.sml.R;
+import com.example.stas.sml.customView.bottomSheet.GoogleMapsBottomSheetBehavior;
+import com.example.stas.sml.presentation.base.ErrorHandler;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.List;
+import javax.inject.Inject;
 
+@RuntimePermissions
+public class MapsActivity extends AppCompatActivity implements MapsContract.MapView, OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener{
 
-public class MapsActivity extends AppCompatActivity{
+    private GoogleMap mMap;
+    private Marker marker;
 
+   @Inject
+   MapsContract.Presenter presenter;
+   @Inject
+   ErrorHandler errorHandler;
 
-    /*@Inject
-    MapsContract.Presenter presenter;
-    @Inject
-    ErrorHandler errorHandler;*/
+   @BindView(R.id.bottom_navigation) BottomNavigationView bottomNavigation;
 
-    @BindView(R.id.bottom_navigation) BottomNavigationView bottomNavigation;
-
+    //BottomSheet
+    private GoogleMapsBottomSheetBehavior behavior;
+    @BindView(R.id.bottomsheet) View bottomsheet;
+    @BindView(R.id.parallax) SliderLayout parallax;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-       //  App.getInstance().addMapsComponent().injectMapsActivity(this);
+        App.getInstance().addMapsComponent().injectMapsActivity(this);
         ButterKnife.bind(this);
+        presenter.attachView(this);
+        setUpMap();
+        presenter.checkNetworkConnection();
+        behavior = GoogleMapsBottomSheetBehavior.from(bottomsheet);
+        behavior.setParallax(parallax);
+
+        bottomsheet.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // set the height of the parallax to fill the gap between the anchor and the top of the screen
+                CoordinatorLayout.LayoutParams layoutParams = new CoordinatorLayout.LayoutParams(parallax.getMeasuredWidth(), behavior.getAnchorOffset());
+                parallax.setLayoutParams(layoutParams);
+                bottomsheet.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+        behavior.setBottomSheetCallback(new GoogleMapsBottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, @GoogleMapsBottomSheetBehavior.State int newState) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
 
         bottomNavigation.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -47,23 +101,96 @@ public class MapsActivity extends AppCompatActivity{
                                 //do places
                                 break;
                             case R.id.action_map:
-                                MapsFragment mapFragment = new MapsFragment();
-                                FragmentManager fm = getSupportFragmentManager();
-                                FragmentTransaction transaction = fm.beginTransaction();
-                                transaction.replace(R.id.fragment_container, mapFragment);
-                                transaction.commit();
-
+                               //do map
+                                setUpMap();
                                 Log.d("map", "map itme clicked");
                                 break;
                         }
                         return true;
                     }
                 });
-
     }
 
-   /* @Override
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        presenter.detachView();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MapsActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        setMarker(latLng);
+        MapsActivityPermissionsDispatcher.setMarkerWithPermissionCheck(this, latLng);
+        behavior.setHideable(true);
+        behavior.setState(GoogleMapsBottomSheetBehavior.STATE_HIDDEN);
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        setMarker(latLng);
+        MapsActivityPermissionsDispatcher.setMarkerWithPermissionCheck(this, latLng);
+        behavior.setState(GoogleMapsBottomSheetBehavior.STATE_COLLAPSED);
+        behavior.setHideable(false);
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        presenter.loadVenueId(latLng);
+    }
+
+    public void showSlider(List<String> urls) {
+        TextSliderView textSliderView = new TextSliderView(this);
+        for(String url : urls){
+            textSliderView
+                    .image(url)
+                    .setScaleType(BaseSliderView.ScaleType.Fit);
+            parallax.addSlider(textSliderView);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        behavior.anchorMap(mMap);
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
+    }
+
+    @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    void setUpMap(){
+        final MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+    }
+
+    @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    void setMarker(LatLng latLng){
+        if (marker != null) {
+            marker.remove();
+        }
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Custom location");
+        marker = mMap.addMarker(markerOptions);
+    }
+
+    @OnPermissionDenied(Manifest.permission.ACCESS_FINE_LOCATION)
+    void showDeniedForMap(){
+        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.ACCESS_FINE_LOCATION)
+    void showNeverAskForMap(){
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
+    }
+
+    @Override
     public void showError(String errorMessage) {
         Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
-    }*/
+    }
 }
---
+++ app/src/main/java/com/example/stas/sml/presentation/feature/map/PlacesActivity.java
@@ -0,0 +1,15 @@
+package com.example.stas.sml.presentation.feature.map;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.stas.sml.R;
+
+public class PlacesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places);
+    }
+}
